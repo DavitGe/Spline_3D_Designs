c012f62b099e4bfb132b5e1bcc5292328b2d67c0
Davit_Gelovani <davitgelovani@Davits-MacBook-Air.local>
2023-02-06 23:28:41 +0400

    ReactDOM import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
+    <div className="flex w-screen h-screen item-center justify-center">
+      <p className="text-2xl text-blue-500">Hello world</p>
     </div>
   );
 }
