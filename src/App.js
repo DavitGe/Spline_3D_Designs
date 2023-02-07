26f9aa7ae6f56a56bdf9ae84a71514793b97e83f
Davit_Gelovani <davitgelovani@Davits-MacBook-Air.local>
2023-02-07 09:26:28 +0400

    Playing garden

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Spline from "@splinetool/react-spline";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen	">
-      <Spline scene="https://prod.spline.design/kX3p6NiT82Wi8xXl/scene.splinecode" />
+    <div className="flex h-screen">
+      {/* <Spline scene="https://prod.spline.design/kX3p6NiT82Wi8xXl/scene.splinecode" /> */}
+      <Spline scene="https://prod.spline.design/AhXTHEpBKG-P5mjb/scene.splinecode" />
     </div>
   );
 }
